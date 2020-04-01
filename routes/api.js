fc4b50fbc08069148674266fe8f19f2a6bd5b936
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-04-01 14:38:38 -0400

    updated index

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const Transaction = require('../models/transaction.js');
 
-router.post('/api/transaction/all', ({ body }, res) => {
+router.post('/api/transaction', ({ body }, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
@@ -21,7 +21,7 @@ router.post('/api/transaction/bulk', ({ body }, res) => {
     });
 });
 
-router.get('/api/transaction/all', (req, res) => {
+router.get('/api/transaction', (req, res) => {
   Transaction.find({})
     .sort({ date: -1 })
     .then(dbTransaction => {
